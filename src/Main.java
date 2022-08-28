c38184d491d8a41c7d34624aa39598e331747cfd
Evgeniy Malygin <black2001@inbox.ru>
2022-08-28 16:40:49 +0300

    Выполнено задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
+    public static void main(String[] args) {
+        //Задание 1
+        System.out.println("========== Задание 1 ==========");
+        int [] arrBuh = generateRandomArray();
+        int summaMonth = 0;
+        for (int i = 0; i < arrBuh.length; i++) {
+            summaMonth += arrBuh[i];
+        }
+        System.out.printf("Сумма трат за месяц составила %s рублей \n", summaMonth);
+
+
+    }
+
 }
